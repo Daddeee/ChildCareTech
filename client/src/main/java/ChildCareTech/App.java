3b3cb11d856b452223344ff55a71bfa9336c7cfa
Daddeee <daddemc@gmail.com>
2018-03-05 16:30:29 +0100

    Project setup

---
+++ client/src/main/java/ChildCareTech/App.java
@@ -0,0 +1,13 @@
+package ChildCareTech;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
