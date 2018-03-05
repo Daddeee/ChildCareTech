1a47fbf091eee3df520f12612767ad7828d3d586
Daddeee <daddemc@gmail.com>
2018-03-05 16:37:08 +0100

    Server hibernate setup

--- server/src/main/java/ChildCareTech/App.java
+++ server/src/main/java/ChildCareTech/App.java
@@ -1,13 +1,18 @@
 package ChildCareTech;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+    private static SessionFactory factory;
+    public static void main(String[] args) {
+
+        try {
+            factory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Failed to create sessionFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
     }
+
 }
