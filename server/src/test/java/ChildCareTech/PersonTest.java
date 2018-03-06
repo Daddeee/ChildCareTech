bcdefcef67b26adabdae1747cadea69c3d0519a4
Daddeee <daddemc@gmail.com>
2018-03-06 10:14:52 +0100

    Person entity [2]

--- server/src/test/java/ChildCareTech/PersonTest.java
+++ server/src/test/java/ChildCareTech/PersonTest.java
@@ -55,4 +55,69 @@ public class PersonTest extends AbstractCRUDTest {
 
         assertTrue(obj.equals(p));
     }
+
+    @Override
+    protected void updateObject() {
+        Transaction tx = null;
+        Person p = new Person("fisccodetest",
+                "name_updated",
+                "surname",
+                new Date(System.currentTimeMillis()),
+                Person.Sex.MALE,
+                "addr",
+                "numb");
+
+        session = sessionFactory.openSession();
+
+        try {
+            tx = session.beginTransaction();
+            session.update(p);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+            fail(e.getMessage());
+        } finally {
+            session.close();
+        }
+
+        obj = p;
+
+        readObject();
+    }
+
+    @Override
+    protected void destroyObject() {
+        session = sessionFactory.openSession();
+        Transaction tx = null;
+
+        try {
+            tx = session.beginTransaction();
+            session.delete(obj);
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+            fail(e.getMessage());
+        } finally {
+            session.close();
+        }
+
+        session = sessionFactory.openSession();
+        tx = null;
+        Person p = null;
+        try {
+            tx = session.beginTransaction();
+            p = (Person) session.get(Person.class, obj.getFiscalCode());
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx!=null) tx.rollback();
+            e.printStackTrace();
+            fail(e.getMessage());
+        } finally {
+            session.close();
+        }
+
+        assertTrue(p == null);
+    }
 }
