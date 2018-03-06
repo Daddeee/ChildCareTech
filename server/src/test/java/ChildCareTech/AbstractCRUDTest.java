4093394e22727e88cb8ed581eb221a4af3ec4002
Daddeee <daddemc@gmail.com>
2018-03-06 09:10:37 +0100

    Minor changes

---
+++ server/src/test/java/ChildCareTech/AbstractCRUDTest.java
@@ -0,0 +1,39 @@
+package ChildCareTech;
+
+import junit.framework.TestCase;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public abstract class AbstractCRUDTest extends TestCase {
+    protected SessionFactory sessionFactory;
+    protected Session session = null;
+
+    protected abstract void createObject();
+    protected abstract void readObject();
+
+    public void testCRUD(){
+        createObject();
+        readObject();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        // setup the session factory
+
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class);
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MariaDBDialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.mariadb.jdbc.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost/test?createDatabaseIfNotExist=true");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        sessionFactory.close();
+    }
+}
