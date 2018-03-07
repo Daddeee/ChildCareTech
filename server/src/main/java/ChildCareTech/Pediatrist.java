95697ef626c96270bd2a5cbaeae3952f637c4e57
Daddeee <daddemc@gmail.com>
2018-03-07 15:24:44 +0100

    Pediatrist - Kid relationship

--- server/src/main/java/ChildCareTech/Pediatrist.java
+++ server/src/main/java/ChildCareTech/Pediatrist.java
@@ -1,10 +1,24 @@
 package ChildCareTech;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @DiscriminatorValue("1")
 public class Pediatrist extends Adult {
+
+    @OneToMany(mappedBy = "pediatrist")
+    private Set<Kid> kids;
+
     public Pediatrist() { super(); }
     public Pediatrist(Person person) { super(person); }
+
+    public Set<Kid> getKids() {
+        return new HashSet<>(kids);
+    }
+
+    private void setKids (Set<Kid> kids) {
+        this.kids = kids;
+    }
 }
