97ced1977b6f7a438d8a85a5a129db2b1f4af77b
Daddeee <daddemc@gmail.com>
2018-03-07 18:22:18 +0100

    Kid entity test

--- server/src/test/java/ChildCareTech/KidTest.java
+++ server/src/test/java/ChildCareTech/KidTest.java
@@ -1,4 +1,49 @@
 package ChildCareTech;
 
-public class KidTest {
+import org.hibernate.JDBCException;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.Test;
+
+import javax.persistence.PersistenceException;
+import java.sql.Date;
+
+public class KidTest extends AbstractCRUDTest<Kid> {
+    @Override
+    protected void createObject() {
+
+    }
+
+    @Override
+    protected void readObject() {
+
+    }
+
+    @Override
+    protected void updateObject() {
+
+    }
+
+    @Override
+    protected void destroyObject() {
+
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void testCheckConstraint(){
+        session = sessionFactory.openSession();
+        Person p = new Person("test",
+                "firstname",
+                "lastname",
+                new Date(System.currentTimeMillis()),
+                Person.Sex.MALE,
+                "addr",
+                "333");
+
+        Kid k = new Kid(p, null, null, null);
+
+        Transaction tx = session.beginTransaction();
+        session.save(k);
+        tx.commit();
+    }
 }
