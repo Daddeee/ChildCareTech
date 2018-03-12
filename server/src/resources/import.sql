4e8e58cfaff172986d97ed29d41342ab98c925b5
Fante <adri-1996@hotmail.it>
2018-03-12 22:57:44 +0100

    no message

--- server/src/resources/import.sql
+++ server/src/resources/import.sql
@@ -1,5 +1,6 @@
---DANGER! instruction in one line, no new line queries
+--DANGER! instruction in one line, no multi-line queries
 
 CREATE TABLE prova_import (id INT PRIMARY KEY);
 INSERT INTO prova_import (id) VALUES (1);
 INSERT INTO prova_import (id) VALUES (2);
+DROP TABLE prova_import;
