8cb0470e7f001200543f2a0790c6eab3ec5cc39d
Daddeee <daddemc@gmail.com>
2018-03-14 16:22:39 +0100

    Dish entity equals + hashcode

--- server/src/main/java/ChildCareTech/model/Dish.java
+++ server/src/main/java/ChildCareTech/model/Dish.java
@@ -1,16 +1,23 @@
 package ChildCareTech.model;
 
+import org.hibernate.annotations.ColumnDefault;
+
 import javax.persistence.*;
 import java.util.Set;
 import java.util.HashSet;
 
 @javax.persistence.Entity
-@Table(name = "disces")
+@Table(name = "dishes",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"name", "menu_id"})
+)
 public class Dish implements iEntity<Dish, Integer> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(nullable = false)
+    private String name;
 
     @ManyToOne(targetEntity = Menu.class)
     @JoinColumn(nullable = false)
@@ -21,8 +28,13 @@ public class Dish implements iEntity<Dish, Integer> {
     private Set<Food> foods;
 
     public Dish() { }
-    public Dish(Menu menu) { this.menu = menu; }
-    public Dish(Menu menu, Set<Food> foods) {
+    public Dish(String name, Menu menu) {
+        this.name = name;
+        this.menu = menu;
+    }
+
+    public Dish(String name, Menu menu, Set<Food> foods) {
+        this.name = name;
         this.menu = menu;
         this.foods = foods;
     }
@@ -41,5 +53,26 @@ public class Dish implements iEntity<Dish, Integer> {
     public Set<Food> getFoods() { return new HashSet<>(foods); }
 
     public void setFoods(Set<Food> foods) { this.foods = foods; }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return (name + menu.getPrimaryKey()).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof Dish)) return false;
+        return this.name.equals(((Dish) o).name) &&
+                this.menu.equals(((Dish) o).menu);
+    }
 
 }
