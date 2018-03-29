9bd0bfe9143d01a4c9a9b9cdaed16c755bbf7a49
Daddeee <daddemc@gmail.com>
2018-03-29 15:12:56 +0200

    PersonDTO interface fix

--- client/src/main/java/ChildCareTech/common/PersonDTO.java
+++ client/src/main/java/ChildCareTech/common/PersonDTO.java
@@ -1,24 +1,25 @@
 package ChildCareTech.common;
 
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public interface PersonDTO extends Remote {
-    public String getFirstName() throws RemoteException;
-    public String getLastName() throws RemoteException;
-    public String getFiscalCode() throws RemoteException;
-    public LocalDate getBirthDate() throws RemoteException;
-    public Sex getSex() throws RemoteException;
-    public String getAddress() throws RemoteException;
-    public String getPhoneNumber() throws RemoteException;
 
-    public void setFirstName(String firstName) throws RemoteException;
-    public void setLastName(String lastName) throws RemoteException;
-    public void setFiscalCode(String fiscalCode) throws RemoteException;
-    public void setBirthDate(LocalDate birthDate) throws RemoteException;
-    public void setSex(Sex sex) throws RemoteException;
-    public void setAddress(String address) throws RemoteException;
-    public void setPhoneNumber(String phoneNumber) throws RemoteException;
+public interface PersonDTO extends Serializable {
+    String getFirstName();
+    String getLastName();
+    String getFiscalCode();
+    LocalDate getBirthDate();
+    Sex getSex();
+    String getAddress();
+    String getPhoneNumber();
+
+    void setFirstName(String firstName);
+    void setLastName(String lastName);
+    void setFiscalCode(String fiscalCode);
+    void setBirthDate(LocalDate birthDate);
+    void setSex(Sex sex);
+    void setAddress(String address);
+    void setPhoneNumber(String phoneNumber);
 }
+
