f07b0b9a87b5b154597a05a45fa07a8beee624b6
Daddeee <daddemc@gmail.com>
2018-04-08 13:32:44 +0200

    add route with trips

--- client/src/main/java/ChildCareTech/controller/AddTripController.java
+++ client/src/main/java/ChildCareTech/controller/AddTripController.java
@@ -7,16 +7,20 @@ import ChildCareTech.services.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 
 
 public class AddTripController {
@@ -30,19 +34,44 @@ public class AddTripController {
     private DatePicker arrDateField;
     @FXML
     private Label alertLabel;
+    @FXML
+    private TextField routeNumberField;
+    @FXML
+    private TextField departureLocationField;
+    @FXML
+    private TextField arrivalLocationField;
 
     @FXML
-    private TableView<RouteDTO> routesTable;
-    @FXML
-    private TableColumn<RouteDTO, String> departureLocationColumn;
-    @FXML
-    private TableColumn<RouteDTO, String> arrivalLocationColumn;
+    private TableView<TempRouteData> routesTable;
 
-    private ObservableList<TripDTO> routes = FXCollections.observableArrayList();
+    private ObservableList<TempRouteData> routes = FXCollections.observableArrayList();
+    private int maxRouteNumber = 1;
+
+    private void initialize(){
+        routesTable.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if(keyEvent.getCode() == KeyCode.CANCEL) {
+                    TempRouteData removed = routesTable.getSelectionModel().getSelectedItem();
+                    routes.remove(removed);
+
+                    maxRouteNumber--;
+                    for(int i = 0; i < maxRouteNumber; i++)
+                        routes.get(i).routeNumber = i + 1;
+
+                    routesTable.getItems().clear();
+                    routesTable.getItems().addAll(routes);
+                }
+            }
+        });
+    }
 
     @FXML
     public void addRouteButtonAction(ActionEvent e) throws IOException{
-
+        TempRouteData r = new TempRouteData(maxRouteNumber, departureLocationField.getText(), arrivalLocationField.getText());
+        maxRouteNumber++;
+        routes.add(r);
+        routesTable.getItems().add(r);
     }
 
     @FXML
@@ -62,7 +91,10 @@ public class AddTripController {
         LocalDate depDate = depDateField.getValue();
         LocalDate arrDate = arrDateField.getValue();
 
-        TripDTO tripDTO = new TripDTO(meta, note, depDate, arrDate, null, null);
+        TripDTO tripDTO = new TripDTO(meta, note, depDate, arrDate, new HashSet<>(), null);
+
+        for(TempRouteData r : routes)
+            tripDTO.getRoutes().add(r.getRouteDTO(tripDTO));
 
         try {
             SessionService.getSession().saveTrip(tripDTO);
@@ -83,5 +115,23 @@ public class AddTripController {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static class TempRouteData{
+        public int routeNumber;
+        public String departureLocation;
+        public String arrivalLocation;
+
+        public TempRouteData(int routeNumber, String departureLocation, String arrivalLocation){
+            this.routeNumber = routeNumber;
+            this.departureLocation = departureLocation;
+            this.arrivalLocation = arrivalLocation;
+        }
+
+        public int getRouteNumber() { return routeNumber; }
+        public String getDepartureLocation() { return departureLocation; }
+        public String getArrivalLocation() { return arrivalLocation; }
+
+        public RouteDTO getRouteDTO(TripDTO t) { return new RouteDTO(t, routeNumber, departureLocation, arrivalLocation); }
     }
 }
