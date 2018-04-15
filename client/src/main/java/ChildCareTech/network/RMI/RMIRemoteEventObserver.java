fd599476039fc946a1fa2deac4a4fc88190fa548
Daddeee <daddemc@gmail.com>
2018-04-15 23:38:13 +0200

    E' VIVO

--- client/src/main/java/ChildCareTech/network/RMI/RMIRemoteEventObserver.java
+++ client/src/main/java/ChildCareTech/network/RMI/RMIRemoteEventObserver.java
@@ -3,7 +3,13 @@ package ChildCareTech.network.RMI;
 import ChildCareTech.common.DTO.EventDTO;
 import ChildCareTech.common.RemoteEventObserver;
 
-public class RMIRemoteEventObserver implements RemoteEventObserver {
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIRemoteEventObserver extends UnicastRemoteObject implements RemoteEventObserver, Serializable {
+    public RMIRemoteEventObserver() throws RemoteException {}
+
     @Override
     public void update(EventDTO eventDTO) {
         System.out.println(eventDTO.getName());
--- server/src/main/java/ChildCareTech/common/RemoteEventObserver.java
+++ server/src/main/java/ChildCareTech/common/RemoteEventObserver.java
@@ -4,7 +4,6 @@ import ChildCareTech.common.DTO.EventDTO;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Observer;
 
 public interface RemoteEventObserver extends Remote {
     void update(EventDTO eventDTO) throws RemoteException;
