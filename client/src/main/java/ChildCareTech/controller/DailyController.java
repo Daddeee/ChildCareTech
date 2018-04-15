7befcdee0615bfc374c4a1b7b51ff308dc212d0e
Daddeee <daddemc@gmail.com>
2018-04-15 20:12:38 +0200

    Better Daiyl window impl (reverted from commit 30022d447f2791833042096be55ab8c380676ff3)

--- client/src/main/java/ChildCareTech/controller/DailyController.java
+++ client/src/main/java/ChildCareTech/controller/DailyController.java
@@ -1,69 +1,18 @@
 package ChildCareTech.controller;
 
-import ChildCareTech.common.DTO.WorkDayDTO;
-import ChildCareTech.services.MainSceneManager;
 import ChildCareTech.services.SessionService;
-import ChildCareTech.utils.DailyEventData;
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.util.Duration;
-
-import java.io.IOException;
-import java.time.LocalTime;
 
 public class DailyController {
     @FXML
-    protected Label clockLabel;
-    @FXML
-    protected Label dateLabel;
-    @FXML
-    protected TableView<DailyEventData> dailyEventDataTable;
-
-    private WorkDayDTO currentWorkDay;
+    protected Label test;
 
     public void initialize(){
-        try{
-            currentWorkDay = SessionService.getSession().getCurrentWorkDay();
-            dateLabel.setText(currentWorkDay.getDate().toString());
-        }catch (Exception e){
+        try {
+            test.setText(SessionService.getSession().getCurrentWorkDay().getDate().toString());
+        } catch (Exception e){
             e.printStackTrace();
         }
-        refreshTable();
-    }
-
-    @FXML
-    protected void backButtonAction(ActionEvent e){
-        try{
-            MainSceneManager.loadHome();
-        } catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
-
-    private void refreshTable() {
-        //if(currentWorkDay.isHoliday()) return;
-
-        dailyEventDataTable.getItems().add(
-                new DailyEventData(
-                    "Ingresso",
-                        currentWorkDay.getEntryTime().toString(),
-                        currentWorkDay.getEntryTime().plusMinutes(10).toString(),
-                        DailyEventData.findStatus(currentWorkDay.getEntryTime(), currentWorkDay.getEntryTime().plusMinutes(10))
-                )
-        );
-
-        dailyEventDataTable.getItems().add(
-                new DailyEventData(
-                        "Uscita",
-                        currentWorkDay.getExitTime().toString(),
-                        currentWorkDay.getExitTime().plusMinutes(10).toString(),
-                        DailyEventData.findStatus(currentWorkDay.getExitTime(), currentWorkDay.getExitTime().plusMinutes(10))
-                )
-        );
     }
 }
