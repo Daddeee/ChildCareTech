0e4232e1330b7c3c9932412bdb84107fb48a91df
Daddeee <daddemc@gmail.com>
2018-04-16 16:16:25 +0200

    Generating basic events when generating workdays

--- server/src/main/java/ChildCareTech/utils/WorkDaysGenerationUtil.java
+++ server/src/main/java/ChildCareTech/utils/WorkDaysGenerationUtil.java
@@ -1,7 +1,9 @@
 package ChildCareTech.utils;
 
 import ChildCareTech.common.DTO.DayGenerationSettingsDTO;
+import ChildCareTech.common.EventStatus;
 import ChildCareTech.model.DAO.WorkDayDAO;
+import ChildCareTech.model.entities.Event;
 import ChildCareTech.model.entities.WorkDay;
 import ChildCareTech.utils.exceptions.ValidationFailedException;
 import org.hibernate.Session;
@@ -9,7 +11,9 @@ import org.hibernate.Transaction;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class WorkDaysGenerationUtil {
     private DayGenerationSettingsDTO settings;
@@ -23,6 +27,8 @@ public class WorkDaysGenerationUtil {
     public void generateDays(){
         LocalDate start = LocalDate.now().with(DayOfWeek.MONDAY);
         LocalDate end = start.plusDays(settings.getWeekNumber() * 7);
+        WorkDay temp;
+        Set<Event> events;
 
         Map<DayOfWeek, Boolean> holidayMap = settings.getAreHolidays();
 
@@ -31,13 +37,38 @@ public class WorkDaysGenerationUtil {
         workDayDAO.setSession(session);
         try {
             tx = session.beginTransaction();
-            for (; !start.isAfter(end); start = start.plusDays(1))
-                workDayDAO.create(new WorkDay(
+            for (; !start.isAfter(end); start = start.plusDays(1)) {
+                temp = new WorkDay(
                         start,
                         settings.getEntryTime(),
                         settings.getExitTime(),
                         holidayMap.get(start.getDayOfWeek())
-                ));
+                );
+
+                events = new HashSet<>();
+                if(!holidayMap.get(start.getDayOfWeek())){
+                    events.add(new Event(
+                                    "Ingresso",
+                                    temp,
+                                    settings.getEntryTime(),
+                                    settings.getEntryTime().plusMinutes(10),
+                                    EventStatus.WAIT
+                            )
+                    );
+
+                    events.add(new Event(
+                                    "Uscita",
+                                    temp,
+                                    settings.getExitTime(),
+                                    settings.getExitTime().plusMinutes(10),
+                                    EventStatus.WAIT
+                            )
+                    );
+                }
+
+                temp.setEvents(events);
+                workDayDAO.create(temp);
+            }
             tx.commit();
         } catch (Exception ex){
             if(tx!=null) tx.rollback();
