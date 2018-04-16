ea2d9b6319aa43c3fd82236d7ef6171ad162c602
Daddeee <daddemc@gmail.com>
2018-04-16 14:04:26 +0200

    Small fix

--- server/src/main/java/ChildCareTech/model/DAO/TripDAO.java
+++ server/src/main/java/ChildCareTech/model/DAO/TripDAO.java
@@ -13,6 +13,7 @@ public class TripDAO extends AbstractGenericDAO<Trip, Integer> {
     public void initializeLazyRelations(Trip trip){
         initializeRouteRelation(trip);
         initializeTripPartecipationRelation(trip);
+        initializeBusRelation(trip);
     }
 
     public void initializeRouteRelation(Trip trip){
@@ -22,4 +23,6 @@ public class TripDAO extends AbstractGenericDAO<Trip, Integer> {
     public void initializeTripPartecipationRelation(Trip trip){
         Hibernate.initialize(trip.getTripPartecipations());
     }
+
+    public void initializeBusRelation(Trip trip) { Hibernate.initialize(trip.getBuses()); }
 }
