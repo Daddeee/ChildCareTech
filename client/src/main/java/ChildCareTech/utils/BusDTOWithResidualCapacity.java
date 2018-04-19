589cad99db8a3fb47475d02ca22e6c84bfd7d361
Daddeee <daddemc@gmail.com>
2018-04-19 22:36:42 +0200

    BusDTO wrapper to manage residual capacity

--- client/src/main/java/ChildCareTech/utils/BusDTOWithResidualCapacity.java
+++ client/src/main/java/ChildCareTech/utils/BusDTOWithResidualCapacity.java
@@ -1,4 +1,45 @@
 package ChildCareTech.utils;
 
-public class BusDTOWithResidualCapacity {
+import ChildCareTech.common.DTO.BusDTO;
+import ChildCareTech.common.DTO.TripDTO;
+import ChildCareTech.common.DTO.TripPartecipationDTO;
+
+import java.util.Set;
+
+public class BusDTOWithResidualCapacity extends BusDTO {
+    private int residualCapacity;
+
+    public BusDTOWithResidualCapacity(int id, String licensePlate, Set<TripPartecipationDTO> tripPartecipations, Set<TripDTO> trips, int capacity, int residualCapacity){
+        super(id, licensePlate, tripPartecipations, trips, capacity);
+        this.residualCapacity = residualCapacity;
+    }
+
+    public int getResidualCapacity() {
+        return residualCapacity;
+    }
+
+    public void setResidualCapacity(int residualCapacity) {
+        this.residualCapacity = residualCapacity;
+    }
+
+    public BusDTO getBusDTO(){
+        return new BusDTO(
+                getId(),
+                getLicensePlate(),
+                getTripPartecipations(),
+                getTrips(),
+                getCapacity()
+        );
+    }
+
+    public static BusDTOWithResidualCapacity buildFromBusDTO(BusDTO busDTO){
+        return new BusDTOWithResidualCapacity(
+                busDTO.getId(),
+                busDTO.getLicensePlate(),
+                busDTO.getTripPartecipations(),
+                busDTO.getTrips(),
+                busDTO.getCapacity(),
+                busDTO.getCapacity()
+        );
+    }
 }
