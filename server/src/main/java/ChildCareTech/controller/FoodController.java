d9e3bdd844ab06f4ff2acac21e022b5c4c322736
Daddeee <daddemc@gmail.com>
2018-05-11 14:52:27 +0200

    RMIUserSession refactoring part 3 (Food controller)

---
+++ server/src/main/java/ChildCareTech/controller/FoodController.java
@@ -0,0 +1,149 @@
+package ChildCareTech.controller;
+
+import ChildCareTech.common.DTO.FoodDTO;
+import ChildCareTech.common.DTO.PersonDTO;
+import ChildCareTech.common.exceptions.AddFailedException;
+import ChildCareTech.common.exceptions.UpdateFailedException;
+import ChildCareTech.model.DAO.FoodDAO;
+import ChildCareTech.model.DAO.PersonDAO;
+import ChildCareTech.model.entities.Food;
+import ChildCareTech.model.entities.Person;
+import ChildCareTech.utils.DTO.DTOEntityAssembler;
+import ChildCareTech.utils.DTO.DTOFactory;
+import ChildCareTech.utils.HibernateSessionFactoryUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class FoodController {
+    public FoodController() {}
+
+    public Collection<FoodDTO> doGetAvailableFoods(PersonDTO personDTO) {
+        FoodDAO foodDAO = new FoodDAO();
+        PersonDAO personDAO = new PersonDAO();
+        List<FoodDTO> foodsDTOCollection = new ArrayList<>();
+        List<Food> foodsCollection = new ArrayList<>();
+        Person person = DTOEntityAssembler.getEntity(personDTO);
+
+        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
+        Transaction tx = null;
+        foodDAO.setSession(session);
+        personDAO.setSession(session);
+        try{
+            tx = session.beginTransaction();
+
+            foodsCollection = foodDAO.getAvailableFoods(person);
+
+            tx.commit();
+        } catch(HibernateException e){
+            if(tx!=null)tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        for(Food f : foodsCollection)
+            foodsDTOCollection.add(DTOFactory.getDTO(f));
+
+        return foodsDTOCollection;
+    }
+
+    public void doUpdateFood(FoodDTO newFoodDTO) throws UpdateFailedException {
+        FoodDAO foodDAO = new FoodDAO();
+        Food newFood = DTOEntityAssembler.getEntity(newFoodDTO);
+
+        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
+        Transaction tx = null;
+        foodDAO.setSession(session);
+        try{
+            tx = session.beginTransaction();
+
+            foodDAO.update(newFood);
+
+            tx.commit();
+        }catch(IndexOutOfBoundsException e){
+            if(tx!=null) tx.rollback();
+            e.printStackTrace();
+            throw new UpdateFailedException("Non è stato trovato alcun alimento da aggiornare");
+        } catch(Exception e){
+            if(tx!=null) tx.rollback();
+            e.printStackTrace();
+            throw new UpdateFailedException(e.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    public void doRemoveFood(FoodDTO foodDTO) {
+        FoodDAO foodDAO = new FoodDAO();
+        Food food = DTOEntityAssembler.getEntity(foodDTO);
+
+        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
+        Transaction tx = null;
+        foodDAO.setSession(session);
+        try {
+            tx = session.beginTransaction();
+            foodDAO.delete(food);
+            tx.commit();
+        } catch(HibernateException e){
+            if(tx!=null) tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void doSaveFood(FoodDTO foodDTO) throws AddFailedException {
+        Food food = DTOEntityAssembler.getEntity(foodDTO);
+        FoodDAO foodDAO = new FoodDAO();
+
+        Transaction tx = null;
+        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
+        foodDAO.setSession(session);
+        try{
+            tx = session.beginTransaction();
+
+            foodDAO.create(food);
+
+            tx.commit();
+        } catch(Exception ex){
+            if(tx!=null) tx.rollback();
+            ex.printStackTrace();
+            throw new AddFailedException(ex.getMessage());
+        } finally {
+            session.close();
+        }
+    }
+
+    public List<FoodDTO> doGetAllFoods() {
+        FoodDAO foodDAO = new FoodDAO();
+        List<Food> foodsCollection = new ArrayList<>();
+        List<FoodDTO> foodsDTOCollection = new ArrayList<>();
+
+        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
+        Transaction tx = null;
+        foodDAO.setSession(session);
+
+        try{
+            tx = session.beginTransaction();
+
+            foodsCollection = foodDAO.readAll();
+
+            tx.commit();
+        } catch(HibernateException e){
+            if(tx!=null)tx.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+
+        for(Food f : foodsCollection)
+            foodsDTOCollection.add(DTOFactory.getDTO(f));
+
+        return foodsDTOCollection;
+    }
+}
--- server/src/main/java/ChildCareTech/network/RMI/RMIUserSession.java
+++ server/src/main/java/ChildCareTech/network/RMI/RMIUserSession.java
@@ -31,6 +31,7 @@ public class RMIUserSession extends UnicastRemoteObject implements UserSession {
     private DishController dishController;
     private CanteenController canteenController;
     private PersonController personController;
+    private FoodController foodController;
 
     public RMIUserSession(User user) throws RemoteException {
         this.user = user;
@@ -38,6 +39,7 @@ public class RMIUserSession extends UnicastRemoteObject implements UserSession {
         this.dishController = new DishController();
         this.canteenController = new CanteenController();
         this.personController = new PersonController();
+        this.foodController = new FoodController();
     }
 
     @Override
@@ -114,130 +116,25 @@ public class RMIUserSession extends UnicastRemoteObject implements UserSession {
 
     @Override
     public Collection<FoodDTO> getAvailableFoods(PersonDTO personDTO) {
-        FoodDAO foodDAO = new FoodDAO();
-        PersonDAO personDAO = new PersonDAO();
-        List<FoodDTO> foodsDTOCollection = new ArrayList<>();
-        List<Food> foodsCollection = new ArrayList<>();
-        Person person = DTOEntityAssembler.getEntity(personDTO);
-
-        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
-        Transaction tx = null;
-        foodDAO.setSession(session);
-        personDAO.setSession(session);
-        try{
-            tx = session.beginTransaction();
-
-            foodsCollection = foodDAO.getAvailableFoods(person);
-
-            tx.commit();
-        } catch(HibernateException e){
-            if(tx!=null)tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        for(Food f : foodsCollection)
-            foodsDTOCollection.add(DTOFactory.getDTO(f));
-
-        return foodsDTOCollection;
+        return foodController.doGetAvailableFoods(personDTO);
     }
 
     public void updateFood(FoodDTO newFoodDTO) throws UpdateFailedException {
-        FoodDAO foodDAO = new FoodDAO();
-        Food newFood = DTOEntityAssembler.getEntity(newFoodDTO);
-
-        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
-        Transaction tx = null;
-        foodDAO.setSession(session);
-        try{
-            tx = session.beginTransaction();
-
-            foodDAO.update(newFood);
-
-            tx.commit();
-        }catch(IndexOutOfBoundsException e){
-            if(tx!=null) tx.rollback();
-            e.printStackTrace();
-            throw new UpdateFailedException("Non è stato trovato alcun alimento da aggiornare");
-        } catch(Exception e){
-            if(tx!=null) tx.rollback();
-            e.printStackTrace();
-            throw new UpdateFailedException(e.getMessage());
-        } finally {
-            session.close();
-        }
+        foodController.doUpdateFood(newFoodDTO);
     }
 
     public void removeFood(FoodDTO foodDTO) throws RemoteException {
-        FoodDAO foodDAO = new FoodDAO();
-        Food food = DTOEntityAssembler.getEntity(foodDTO);
-
-        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
-        Transaction tx = null;
-        foodDAO.setSession(session);
-        try {
-            tx = session.beginTransaction();
-            foodDAO.delete(food);
-            tx.commit();
-        } catch(HibernateException e){
-            if(tx!=null) tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
+        foodController.doRemoveFood(foodDTO);
     }
 
     @Override
     public void saveFood(FoodDTO foodDTO) throws AddFailedException {
-        Food food = DTOEntityAssembler.getEntity(foodDTO);
-        FoodDAO foodDAO = new FoodDAO();
-
-        Transaction tx = null;
-        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
-        foodDAO.setSession(session);
-        try{
-            tx = session.beginTransaction();
-
-            foodDAO.create(food);
-
-            tx.commit();
-        } catch(Exception ex){
-            if(tx!=null) tx.rollback();
-            ex.printStackTrace();
-            throw new AddFailedException(ex.getMessage());
-        } finally {
-            session.close();
-        }
+        foodController.doSaveFood(foodDTO);
     }
 
     @Override
     public List<FoodDTO> getAllFoods() {
-        FoodDAO foodDAO = new FoodDAO();
-        List<Food> foodsCollection = new ArrayList<>();
-        List<FoodDTO> foodsDTOCollection = new ArrayList<>();
-
-        Session session = HibernateSessionFactoryUtil.getInstance().openSession();
-        Transaction tx = null;
-        foodDAO.setSession(session);
-
-        try{
-            tx = session.beginTransaction();
-
-            foodsCollection = foodDAO.readAll();
-
-            tx.commit();
-        } catch(HibernateException e){
-            if(tx!=null)tx.rollback();
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-
-        for(Food f : foodsCollection)
-            foodsDTOCollection.add(DTOFactory.getDTO(f));
-
-        return foodsDTOCollection;
+        return foodController.doGetAllFoods();
     }
 
     @Override
