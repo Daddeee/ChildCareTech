35179550fc70d7e9e2be46432b8f508b4bcc493e
Fante <adri-1996@hotmail.it>
2018-05-18 15:08:41 +0200

    new addDish graphics

--- client/src/main/java/ChildCareTech/controller/AddDishController.java
+++ client/src/main/java/ChildCareTech/controller/AddDishController.java
@@ -4,19 +4,16 @@ import ChildCareTech.common.DTO.DishDTO;
 import ChildCareTech.common.DTO.FoodDTO;
 import ChildCareTech.services.*;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.rmi.RemoteException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class AddDishController implements AccessorWindowController{
     @FXML
@@ -24,20 +21,23 @@ public class AddDishController implements AccessorWindowController{
     @FXML
     protected TableView<FoodDTO> availableIngredientsTable;
     @FXML
+    private TableColumn<FoodDTO, String> availableName;
+    @FXML
     protected TableView<FoodDTO> selectedIngredientsTable;
+    @FXML
+    private TableColumn<FoodDTO, String> selectedName;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button removeButton;
 
     private AccessorWindowService accessorWindowService;
+    private ObservableList<FoodDTO> availableFoods = FXCollections.observableArrayList();
+    private ObservableList<FoodDTO> selectedFoods = FXCollections.observableArrayList();
 
     public void initialize() {
-        List<FoodDTO> availableIngredients = Collections.emptyList();
-        try{
-            availableIngredients = SessionService.getSession().getAllFoods();
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-
-        availableIngredientsTable.getItems().clear();
-        availableIngredientsTable.getItems().addAll(availableIngredients);
+        initMenu();
+        initTables();
     }
 
     @FXML
@@ -56,21 +56,74 @@ public class AddDishController implements AccessorWindowController{
 
     @FXML
     protected void addIngredientButtonAction(ActionEvent event){
-        int rowIndex = availableIngredientsTable.getSelectionModel().getFocusedIndex();
+        FoodDTO selected = availableIngredientsTable.getSelectionModel().getSelectedItem();
+        if(selected == null) return;
+        selectedFoods.add(selected);
+        availableFoods.remove(selected);
+        selectedIngredientsTable.getSelectionModel().select(null);
+       /* int rowIndex = availableIngredientsTable.getSelectionModel().getFocusedIndex();
 
         selectedIngredientsTable.getItems().add(availableIngredientsTable.getItems().get(rowIndex));
         availableIngredientsTable.getItems().remove(rowIndex);
+        */
     }
 
     @FXML
     protected void removeIngredientButtonAction(ActionEvent event){
-        int rowIndex = selectedIngredientsTable.getSelectionModel().getFocusedIndex();
+        FoodDTO selected = selectedIngredientsTable.getSelectionModel().getSelectedItem();
+        if(selected == null) return;
+        selectedFoods.remove(selected);
+        availableFoods.add(selected);
+       /* int rowIndex = selectedIngredientsTable.getSelectionModel().getFocusedIndex();
 
         availableIngredientsTable.getItems().add(selectedIngredientsTable.getItems().get(rowIndex));
         selectedIngredientsTable.getItems().remove(rowIndex);
+        */
     }
 
     public void setAccessorWindowService(AccessorWindowService accessorWindowService) {
         this.accessorWindowService = accessorWindowService;
+    }
+
+    private void initMenu() {
+        removeButton.setDisable(true);
+        addButton.setDisable(true);
+
+        selectedIngredientsTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                removeButton.setDisable(false);
+                addButton.setDisable(true);
+            }
+            else {
+                removeButton.setDisable(true);
+                addButton.setDisable(true);
+            }
+        });
+        availableIngredientsTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+                removeButton.setDisable(true);
+                addButton.setDisable(false);
+            }
+            else {
+                removeButton.setDisable(true);
+                addButton.setDisable(true);
+            }
+        });
+    }
+    private void initTables() {
+        List<FoodDTO> availableFoodsList = new ArrayList<>();
+
+        availableName.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getName()));
+        selectedName.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getName()));
+
+        try{
+            availableFoodsList = SessionService.getSession().getAllFoods();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        availableFoods.clear();
+        availableFoods.addAll(availableFoodsList);
+        availableIngredientsTable.setItems(availableFoods);
+        selectedIngredientsTable.setItems(selectedFoods);
     }
 }
