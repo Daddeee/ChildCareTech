48fff643b6fcebef4f80a48d2a4aa2b41e94e328
Daddeee <daddemc@gmail.com>
2018-05-27 14:26:26 +0200

    Javadoc

--- server/src/main/java/ChildCareTech/model/AbstractGenericDAO.java
+++ server/src/main/java/ChildCareTech/model/AbstractGenericDAO.java
@@ -14,45 +14,108 @@ import javax.validation.Validator;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * Represent a generic Data Access Object.
+ * <p>
+ * Provides implementations for the basic CRUD operations.
+ *
+ * @param <T> the type of entity on which this DAO is operating
+ * @param <K> the type of the key of the aforementioned entity
+ */
 public abstract class AbstractGenericDAO<T extends iEntity, K extends Serializable> {
-
     private Class<T> persistentClass;
+
+    /**
+     * The Hibernate Session needed by this dao to perform every operations.
+     * It must be opened and with an active transaction to perform successfully any operation.
+     * The manage of sessions/transaction is job of the developer that uses this class.
+     */
     protected Session session;
+
     private Validator validator;
 
+    /**
+     * Create a new instance of this object and initialize the correspondent Hibernate validator.
+     *
+     * @param clazz the managed entity's class
+     */
     public AbstractGenericDAO(Class<T> clazz) {
         this.persistentClass = clazz;
         this.validator = Validation.buildDefaultValidatorFactory().getValidator();
     }
 
+    /**
+     * Trigger the load of every lazy relationship for the given entity.
+     *
+     * @param obj the provided entity.
+     */
+    public abstract void initializeLazyRelations(T obj);
+
+    /**
+     * @param s the Hibernate {@link #session}.
+     */
     public void setSession(Session s) {
         this.session = s;
     }
 
+    /**
+     * @return the Hibernate {@link #session}
+     */
     protected Session getSession() {
         if (session == null)
             throw new IllegalStateException("Session has not been set on DAO before usage");
         return session;
     }
 
+    /**
+     * @return the managed entity's class
+     */
     public Class<T> getPersistentClass() {
         return persistentClass;
     }
 
+    /**
+     * Save the provided entity in the database.
+     *
+     * @param obj the entity to be saved in the database.
+     * @throws ValidationFailedException if the provided entity does not pass some validations constraints.
+     * @throws org.hibernate.HibernateException if some database constraints are violated.
+     */
     public void create(T obj) throws ValidationFailedException{
         validateEntity(obj);
         session.save(obj);
     }
 
+    /**
+     * Get from the database the entity that has the primary key equals to the provided key.
+     *
+     * @param key the key to fetch from the database
+     * @return the entity with corresponding primary key
+     */
     public T read(K key) {
         return session.get(persistentClass, key);
     }
 
+    /**
+     * Get from the database the entity that has the primary key equals to the primary key of the provided entity.
+     *
+     * @param obj the entity provided
+     * @return the entity with corresponding primary key
+     */
     @SuppressWarnings("unchecked")
     public T read(T obj) {
         return read((K) obj.getPrimaryKey());
     }
 
+    /**
+     * Read a list of entities matching the constraints specified in the params map.
+     * <p>
+     * Each entry of the aforementioned map specify the value for an attribute that an entity must match to be read.
+     * For each entry, a where clause is added to the read query: (String) attribute = (Object) value.
+     *
+     * @param params a map containing the name of the attribute as key and the corresponding value as value.
+     * @return a list of entity matching the specified constraints.
+     */
     @SuppressWarnings("unchecked")
     public List<T> read(Map<String, Object> params) {
         StringBuilder query = new StringBuilder("from " + persistentClass.getName() + " where ");
@@ -71,12 +134,26 @@ public abstract class AbstractGenericDAO<T extends iEntity, K extends Serializab
         return queryToFill.list();
     }
 
+    /**
+     * Read a list of entities matching the constraints specified in the params map.
+     * <p>
+     * Each entry of the aforementioned map specify the value for an attribute that an entity must match to be read.
+     * For each entry, a where clause is added to the read query: (String) attribute = (Object) value.
+     *
+     * @param paramName the name of the attribute.
+     * @param paramValue the value of the attribute.
+     * @return a list of entity matching the specified constraints.
+     */
     public List<T> read(String paramName, Object paramValue) {
         HashMap<String, Object> map = new HashMap<>();
         map.put(paramName, paramValue);
         return read(map);
     }
 
+    /**
+     *
+     * @return a list containing all the entities of this type saved in the database.
+     */
     public List<T> readAll() {
         CriteriaBuilder builder = session.getCriteriaBuilder();
         CriteriaQuery<T> criteria = builder.createQuery(persistentClass);
@@ -85,11 +162,24 @@ public abstract class AbstractGenericDAO<T extends iEntity, K extends Serializab
         return session.createQuery(criteria).getResultList();
     }
 
+    /**
+     * Update the provided entity's state in the database.
+     *
+     * @param updatedObj the entity whose state is going to be saved in the database.
+     * @throws ValidationFailedException if the provided entity does not pass some validations constraints.
+     * @throws org.hibernate.HibernateException if some database constraints are violated.
+     */
     public void update(T updatedObj) throws ValidationFailedException {
         validateEntity(updatedObj);
         session.merge(updatedObj);
     }
 
+    /**
+     * Remove the provided entity from the database.
+     *
+     * @param obj the entity to be removed.
+     * @throws org.hibernate.HibernateException if some database constraints are violated.
+     */
     public void delete(T obj) {
         session.delete(obj);
     }
@@ -106,6 +196,4 @@ public abstract class AbstractGenericDAO<T extends iEntity, K extends Serializab
 
         throw new ValidationFailedException(errorMessage.toString());
     }
-
-    public abstract void initializeLazyRelations(T obj);
 }
--- server/src/main/java/ChildCareTech/model/iEntity.java
+++ server/src/main/java/ChildCareTech/model/iEntity.java
@@ -2,6 +2,16 @@ package ChildCareTech.model;
 
 import java.io.Serializable;
 
+/**
+ * This interface represent a generic Entity.
+ *
+ * @param <T> the entity type.
+ * @param <K> the primary key type.
+ */
 public interface iEntity<T extends iEntity<T, K>, K extends Serializable> {
+
+    /**
+     * @return the entity's primary key.
+     */
     K getPrimaryKey();
 }
